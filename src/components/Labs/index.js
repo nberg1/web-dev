b43cc574244a4213e37e3be37fd28c8b0d9d7096
nberg002 <berg.n@northeastern.edu>
2022-03-10 01:17:23 -0500

    a6 functioning explore page and design sp22

--- src/components/Labs/index.js
+++ src/components/Labs/index.js
@@ -12,7 +12,7 @@ const Labs = () => {
             <Link to="/hello">
                 Hello
             </Link> |
-            <Link to="/tuiter">
+            <Link to="/Tuiter">
                 Tuiter
             </Link>
             <TodoList/>
