77ededc1c28ed8ace8932f9ba5b5b16a5894f687
nberg002 <berg.n@northeastern.edu>
2022-03-20 19:13:59 -0400

    testing change

--- src/components/Tuiter/PostSummaryList/PostSummaryItem.js
+++ src/components/Tuiter/PostSummaryList/PostSummaryItem.js
@@ -5,7 +5,7 @@ import "../Styles/index.css"
 const PostSummaryItem = ({
      post = {
          "topic": "Web Development",
-         "userName": "ReactJS",
+         "userName": "TESTING",
          "time": "2h",
          "title": "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
          "image": "../images/ReactJS.png"
