431bf720a1f18e623174fd50c2a2a8c385574fb8
nberg002 <berg.n@northeastern.edu>
2022-03-27 19:26:32 -0400

    A7 fixing redirect problem sp22

--- src/components/Tuiter/index.js
+++ src/components/Tuiter/index.js
@@ -5,12 +5,12 @@ import NavigationSidebar from "./NavigationSidebar";
 const Tuiter = () => {
     return(
         <div className="row mt-2">
-            <div className="col-2 col-lg-1 col-xl-2">
-                <NavigationSidebar/>
-            </div>
-            <div className="col-10 col-lg-7 col-xl-6">
-                <Outlet/>
-            </div>
+            {/*<div className="col-2 col-lg-1 col-xl-2">*/}
+            {/*    <NavigationSidebar/>*/}
+            {/*</div>*/}
+            {/*<div className="col-10 col-lg-7 col-xl-6">*/}
+            {/*    <Outlet/>*/}
+            {/*</div>*/}
             <div className="d-none d-lg-block col-lg-4 col-xl-4">
                 <h2>Who to follow</h2>
             </div>
