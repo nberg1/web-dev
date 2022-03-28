0a7b4e827708faf747f283b4d2e8a0bc0eb6e71b
nberg002 <berg.n@northeastern.edu>
2022-03-27 22:29:45 -0400

    a7 modifying navigation bar links sp22

--- src/components/Tuiter/NavigationSidebar/index.js
+++ src/components/Tuiter/NavigationSidebar/index.js
@@ -14,60 +14,62 @@ const NavigationSidebar = (
                 </a>
                 <a id="home"
                    className={`nav-link mb-2 text-white d-inline-flex flex-row align-items-center ${active === 'home' ? 'active' : ''}`}>
-                    <i className="fa fa-home fa-1x me-1 p-2 text-white"></i>
-                    <Link className= "text-white text-decoration-none" to="/Tuiter/">
+                    <Link className= "text-white text-decoration-none d-inline-flex flex-row align-items-center" to="/Tuiter/">
+                        <i className="fa fa-home fa-1x me-1 p-2 text-white"></i>
                         <span className="d-none d-xl-block">Home</span>
                     </Link>
                 </a>
                 <a id="explore"
                    className={`nav-link mb-2 text-white d-inline-flex flex-row align-items-center ${active === 'explore' ? 'active' : ''}`}>
-                    <i className="fa fa-hashtag fa-1x me-2 p-2 text-white"></i>
-                    <Link className= "text-white text-decoration-none" to="/Tuiter/explore">
+                    <Link className= "text-white text-decoration-none d-inline-flex flex-row align-items-center" to="/Tuiter/explore">
+                        <i className="fa fa-hashtag fa-1x me-2 p-2 text-white"></i>
                         <span className="d-none d-xl-block">Explore</span>
                     </Link>
                 </a>
                 <a id="notifications"
                    className={`nav-link mb-2 text-white d-inline-flex flex-row align-items-center ${active === 'notifications' ? 'active' : ''}`}>
-                    <i className="fa fa-bell fa-1x me-2 p-2 text-white"></i>
-                    <Link className= "text-white text-decoration-none" to="/Tuiter/NotificationsScreen">
+                    <Link className= "text-white text-decoration-none d-inline-flex flex-row align-items-center" to="/Tuiter/NotificationsScreen">
+                        <i className="fa fa-bell fa-1x me-2 p-2 text-white"></i>
                         <span className="d-none d-xl-block">Notifications</span>
                     </Link>
                 </a>
                 <a id="messages"
                    className={`nav-link mb-2 text-white d-inline-flex flex-row align-items-center ${active === 'messages' ? 'active' : ''}`}>
-                    <i className="fa fa-envelope fa-1x me-2 p-2 text-white"></i>
-                    <Link className= "text-white text-decoration-none" to="/Tuiter/MessagesScreen">
+                    <Link className= "text-white text-decoration-none d-inline-flex flex-row align-items-center" to="/Tuiter/MessagesScreen">
+                        <i className="fa fa-envelope fa-1x me-2 p-2 text-white"></i>
                         <span className="d-none d-xl-block">Messages</span>
                     </Link>
                 </a>
                 <a id="bookmarks"
                    className={`nav-link mb-2 text-white d-inline-flex flex-row align-items-center ${active === 'bookmarks' ? 'active' : ''}`}>
-                    <i className="fa fa-bookmark fa-1x me-2 p-2 text-white"></i>
-                    <Link className= "text-white text-decoration-none" to="/Tuiter/BookmarksScreen">
+                    <Link className= "text-white text-decoration-none d-inline-flex flex-row align-items-center" to="/Tuiter/BookmarksScreen">
+                        <i className="fa fa-bookmark fa-1x me-2 p-2 text-white"></i>
                         <span className="d-none d-xl-block">Bookmarks</span>
                     </Link>
                 </a>
                 <a id="lists"
-                   className={`nav-link mb-2 text-white d-inline-flex flex-row align-items-center ${active === 'lists' ? 'active' : ''}`}
-                   href="../lists.html">
-                    <i className="fa fa-list fa-1x me-2 p-2 text-white"></i>
-                    <span className="d-none d-xl-block">Lists</span>
+                   className={`nav-link mb-2 text-white d-inline-flex flex-row align-items-center ${active === 'lists' ? 'active' : ''}`}>
+                    <Link className= "text-white text-decoration-none d-inline-flex flex-row align-items-center" to="/Tuiter/Lists">
+                        <i className="fa fa-list fa-1x me-2 p-2 text-white"></i>
+                        <span className="d-none d-xl-block">Lists</span>
+                    </Link>
                 </a>
                 <a id="profile"
                    className={`nav-link mb-2 text-white d-inline-flex flex-row align-items-center ${active === 'profile' ? 'active' : ''}`}>
-                    <i className="fa fa-user fa-1x me-2 p-2 text-white"></i>
-                    <Link className= "text-white text-decoration-none" to="/Tuiter/ProfileScreen">
+                    <Link className= "text-white text-decoration-none d-inline-flex flex-row align-items-center" to="/Tuiter/ProfileScreen">
+                        <i className="fa fa-user fa-1x me-2 p-2 text-white d-inline-flex flex-row align-items-center"></i>
                         <span className="d-none d-xl-block">Profile</span>
                     </Link>
                 </a>
                 <a id="more"
-                   className={`nav-link mb-2 text-white d-inline-flex flex-row align-items-center ${active === 'more' ? 'active' : ''}`}
-                   href="../more.html">
-                    <span className="fa-stack me-2">
-                        <i className="fas fa-circle fa-stack-1x ms-md-0 text-white" width="100%" style={{fontSize: "25px"}}/>
-                        <i className="fas fa-ellipsis-h fa-stack-1x" style={{color: "black"}}/>
-                    </span>
-                    <span className="d-none d-xl-block">More</span>
+                   className={`nav-link mb-2 text-white d-inline-flex flex-row align-items-center ${active === 'more' ? 'active' : ''}`}>
+                    <Link className="text-white text-decoration-none flex-row d-inline-flex flex-row align-items-center" to="/Tuiter/more">
+                        <span className="fa-stack me-2">
+                            <i className="fas fa-circle fa-stack-1x ms-md-0 text-white" width="100%" style={{fontSize: "25px"}}/>
+                            <i className="fas fa-ellipsis-h fa-stack-1x" style={{color: "black"}}/>
+                        </span>
+                        <span className="d-none d-xl-block">More</span>
+                    </Link>
                 </a>
                 <button type="button"
                         className="wd-tuit-button rounded-pill btn-primary btn-block text-white p-2">Tuit
