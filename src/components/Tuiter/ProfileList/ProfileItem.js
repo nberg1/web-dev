9eac2d9ae4be4318225f12a0611397c4cf38444a
nberg002 <berg.n@northeastern.edu>
2022-03-28 15:47:06 -0400

    a7 profile using react-redux sp22

--- src/components/Tuiter/ProfileList/ProfileItem.js
+++ src/components/Tuiter/ProfileList/ProfileItem.js
@@ -16,46 +16,42 @@ const ProfileItem = ({profileData}) => {
                     </Link>
                 </a>
                 <div>
-                    <h5 className="mb-0 fw-bold">{profileData.firstName}</h5>
+                    <h5 className="mb-0 fw-bold">{profileData.firstName} {profileData.lastName}</h5>
                     <span>10 Tuits</span>
                 </div>
             </div>
 
             <div>
-                <img className="position-relative" width="100%" height="200px" src="/images/banner.jpg"/>
+                <img className="position-relative" width="100%" height="200px" src={profileData.bannerPicture}/>
                 <img className="rounded-circle border-2 position-relative" width="125px" height="125px"
                      style={{
                          borderStyle: "solid",
                          borderColor: "black",
                          zIndex: 1
                      }}
-                     src="/images/nicole_berg_image.jpg"/>
+                     src={profileData.profilePicture}/>
             </div>
             <div className="position-relative mt-3">
                 <div className="float-end">
                     <button className="bg-black border-dark text-white float-end rounded-pill p-2 ps-3 pe-3">Edit profile</button>
                 </div>
                 <div>
-                    <h5 className="fw-bold mb-0">Nicole Berg</h5>
+                    <h5 className="fw-bold mb-0">{profileData.firstName} {profileData.lastName}</h5>
                 </div>
-                <div className="wd-font-color-gray mb-3">
-                    @nicole
-                </div>
-                <div>
-                    Full time student pursuing a Master's degree in Computer Science.
-                </div>
+                <div className="wd-font-color-gray mb-3">@{profileData.handle}</div>
+                <div>{profileData.bio}</div>
                 <div className="d-flex justify-content-between overflow-visible mt-3 pb-1 pe-4 me-5 pe-5 wd-font-color-gray">
-                    <span><i className="fa fa-map-marker text-dark"></i> Boston, MA</span>
-                    <span><i className="fa fa-birthday-cake text-dark"></i> Born May 28, 1993</span>
-                    <span><i className="fa fa-calendar text-dark"></i> Joined March 2019</span>
+                    <span><i className="fa fa-map-marker text-dark"></i> {profileData.location}</span>
+                    <span><i className="fa fa-birthday-cake text-dark"></i> Born {profileData.dateOfBirth}</span>
+                    <span><i className="fa fa-calendar text-dark"></i> Joined {profileData.dateJoined}</span>
                 </div>
                 <div className="d-flex float-start justify-content-around overflow-visible mt-3 pb-1 pe-4 me-5 pe-5 wd-font-color-gray">
                     <span className="d-flex me-3">
-                        <span className="me-1 text-white fw-bold">312</span>
+                        <span className="me-1 text-white fw-bold">{profileData.followingCount}</span>
                         Following
                     </span>
                     <span className="d-flex me-3">
-                        <span className="me-1 text-white fw-bold">180</span>
+                        <span className="me-1 text-white fw-bold">{profileData.followersCount}</span>
                         Followers
                     </span>
                 </div>
