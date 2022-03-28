8e45509d73110d2fc6e7fa7849efb54d00987035
nberg002 <berg.n@northeastern.edu>
2022-03-27 21:34:30 -0400

    a7 fixed homescreen sp22

--- src/components/Tuiter/WhoToFollowList/index.js
+++ src/components/Tuiter/WhoToFollowList/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import who from "./who.json";
 import WhoToFollowListItem from "./WhoToFollowListItem";
 import "../Styles/explore.css"
 import "../Styles/index.css"
+import {useSelector} from "react-redux";
 
 const WhoToFollowList = () => {
+    const who = useSelector((state) => state);
     return(
         <div className="container border border-0 wd-who-to-follow-bg rounded d-none d-lg-block p-3">
             <div className="mb-4 wd-who-to-follow-bg text-white">
--- src/components/Tuiter/index.js
+++ src/components/Tuiter/index.js
@@ -1,21 +1,26 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
 import NavigationSidebar from "./NavigationSidebar";
-
-
+import whoReducer from "./reducers/who-reducer";
+import WhoToFollowList from "./WhoToFollowList";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+const store = createStore(whoReducer);
 const Tuiter = () => {
     return(
-        <div className="row mt-2">
-            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                <NavigationSidebar/>
+        <Provider store={store}>
+            <div className="row mt-2">
+                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                    <NavigationSidebar/>
+                </div>
+                <div className="col-10 col-md-10 col-lg-7 col-xl-6">
+                    <Outlet/>
+                </div>
+                <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                    <WhoToFollowList/>
+                </div>
             </div>
-            <div className="col-10 col-md-10 col-lg-7 col-xl-6">
-                <Outlet/>
-            </div>
-            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-                <h2>Who to follow</h2>
-            </div>
-        </div>
+        </Provider>
     )
 };
 
